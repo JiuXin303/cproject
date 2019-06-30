96c82848a18168c2eb08bb43913413941f4fac86
chenhaoya <50227375+chenhaoya@users.noreply.github.com>
2019-06-30 23:55:26 +0800

    Update 源.c

--- 源.c
+++ 源.c
@@ -3,61 +3,89 @@
 #include <time.h>
 int xingqi(int y,int m,int d)
 {
-	int year = y;
-	if (m == 1 || m == 2)
-	{
-		m = m + 12;
-		y = y - 1;
-	}
-	int week = (d + 2 * m + 3 * (m + 1) / 5 + y + y / 4 - y / 100 + y / 400) % 7;
-	switch (week)
-	{
-	case 0: printf("ĞÇÆÚÒ» "); break;
-	case 1: printf("ĞÇÆÚ¶ş "); break;
-	case 2: printf("ĞÇÆÚÈı "); break;
-	case 3: printf("ĞÇÆÚËÄ "); break;
-	case 4: printf("ĞÇÆÚÎå "); break;
-	case 5: printf("ĞÇÆÚÁù "); break;
-	case 6: printf("ĞÇÆÚÈÕ "); break;
-	}
-	return 0;
+        int year = y;
+        if (m == 1 || m == 2)
+        {
+                m = m + 12;
+                y = y - 1;
+        }
+        int week = (d + 2 * m + 3 * (m + 1) / 5 + y + y / 4 - y / 100 + y / 400) % 7;
+        switch (week)
+        {
+        case 0: printf("æ˜ŸæœŸä¸€\t "); break;
+        case 1: printf("æ˜ŸæœŸäºŒ\t "); break;
+        case 2: printf("æ˜ŸæœŸä¸‰\t "); break;
+        case 3: printf("æ˜ŸæœŸå››\t "); break;
+        case 4: printf("æ˜ŸæœŸäº”\t "); break;
+        case 5: printf("æ˜ŸæœŸå…­\t "); break;
+        case 6: printf("æ˜ŸæœŸæ—¥\t "); break;
+        }
+        return 0;
 }
 int rili(int year,int m)
 {
-	int a[2][12] =
-	{
-		{31,29,31,30,31,30,31,31,30,31,30,31},
-		{31,28,31,30,31,30,31,31,30,31,30,31}
-	};
-	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0 && year % 3200 != 0) || year % 172800 == 0)
-	{
-		printf("%dÊÇÈòÄê\n", year);
-		printf("%dÔÂ:\n", m);
-		int b;
-		for (b=1;b<=a[0][m - 1];b++)
-		{
-			printf("%d", b);
-			xingqi(year, m, b);
-		}
-		return 0;
+        int a[2][12] =
+        {
+                {31,29,31,30,31,30,31,31,30,31,30,31},
+                {31,28,31,30,31,30,31,31,30,31,30,31}
+        };
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0 && year % 3200 != 0) || year % 172800 == 0)
+        {
+                printf("\t\t%då¹´%dæœˆ é—°å¹´\n", year, m);
+                int b;
+                for (b = 1; b <= a[0][m - 1]; b++)
+                {
+                        printf("%då·:", b);
+                        xingqi(year, m, b);
+                }
+                printf("\n");
+                return 0;
     }
-	else;
-	{
-		printf("%dÊÇÆ½Äê\n", year);
-		int c;
-		for (c = 1; c <= 12; c++)
-		{
-			printf("%dÔÂ:", c);
-			printf("%dÌì ", a[1][c - 1]);
-		}
-		printf("\n");
-	}
-	return 0;
+        else;
+        {
+                printf("\t\t%då¹´%dæœˆ å¹³å¹´\n", year, m);
+                int c;
+                for (c = 1; c <= a[1][m - 1]; c++)
+                {
+                        printf("%då·:", c);
+                        xingqi(year, m, c);
+                }
+                printf("\n");
+                return 0;
+        }
+        return 0;
 }
 int main()
 {
-	int a, b;
-	scanf("%d%d", &a, &b);
-	rili(a, b);
-	return 0;
+        printf("\t\tcè¯­è¨€æ—¥å†\n");
+        time_t timep;
+        struct tm *p;
+        time (&timep);
+        p=gmtime(&timep);
+        printf("\t\t%då¹´%dæœˆ%dæ—¥\n",1900+p->tm_year,1+p->tm_mon,p->tm_mday);
+main:
+        printf("");
+        int a, b;
+        printf("è¯·è¾“å…¥å¹´:");
+        scanf("%d", &a);
+        printf("è¯·è¾“å…¥æœˆ:");
+        scanf("%d",&b);
+        while(b>12)
+        {
+        printf("è¾“å…¥é”™è¯¯,è¯·é‡æ–°è¾“å…¥:");
+        scanf("%d",&b);
+        }
+        rili(a, b);
+        printf("\t\t1.ç»§ç»­\n");
+        printf("\t\tå…¶å®ƒæ•°å­—é€€å‡º\n");
+        printf("\t\tè¯·è¾“å…¥ä½ çš„é€‰é¡¹:");
+        int c;
+        scanf("%d",&c);
+        switch (c)
+        {
+        case 1:goto main;break;
+        default:goto exit;break;
+        }
+exit:
+        return 0;
 }
